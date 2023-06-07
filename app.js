781a30f70c20649a21740e1da90b38ec15447297
mmehdiali5 <mmehdiali5@gmail.com>
2023-06-06 20:05:14 -0400

    Authentication Implemented

--- app.js
+++ app.js
@@ -3,10 +3,24 @@ import cors from 'cors'
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import session from "express-session";
+import AuthController from "./users/auth-controller.js";
 
 const app = express()
-app.use(cors())
+app.use(
+    session({
+        secret: "any string",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+app.use(cors({
+        credentials: true,
+        origin: "http://localhost:3000",
+    })
+)
 app.use(express.json())
+AuthController(app)
 HelloController(app)
 UserController(app)
 TuitsController(app)
