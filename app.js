82f5f0724e2937504d1edb714d930ff0aba13c12
mmehdiali5 <mmehdiali5@gmail.com>
2023-06-20 13:57:59 -0400

    Session Fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose.connect(CONNECTION_STRING);
 const MongoDBStore = MongoDBStoreFactory(session);
 const app = express()
 
-app.use(
+/*app.use(
     session({
         secret: "any string",
         resave: false,
@@ -24,7 +24,7 @@ app.use(
             collection: 'sessions'
         })
     })
-);
+);*/
 
 /*app.use(
     session({
@@ -38,7 +38,7 @@ app.use(
     })
 );*/
 
-/*app.use(
+app.use(
     session({
         secret: "any string",
         resave: false,
@@ -49,7 +49,7 @@ app.use(
             secure: true
         }
     })
-);*/
+);
 
 app.use(cors({
         credentials: true,
